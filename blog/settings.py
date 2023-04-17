7dcabc91baebe5e45fdfb5527fed8adf168f5a72
iesusdavila <irdavila@espol.edu.ec>
2023-04-16 22:22:11 -0500

    Importacion de Tailwind para diseños

--- blog/settings.py
+++ blog/settings.py
@@ -26,8 +26,19 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
 
+    'tailwind',
+    'theme',
+
     'posts',
 ]
+
+TAILWIND_APP_NAME = 'theme'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
